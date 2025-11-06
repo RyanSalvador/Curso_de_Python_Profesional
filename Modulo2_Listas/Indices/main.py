ed902c77031f2e050a13e53c5f43b4154710216f
Salvador Guzman <131213382+RyanSalvador@users.noreply.github.com>
2025-11-05 22:15:10 -0600

    Modulo 2 listas

---
+++ Modulo2_Listas/Indices/main.py
@@ -0,0 +1,13 @@
+courses = ["Python", "Django", "Flask", "Ruby", "MongoDB"] #strings
+
+
+print(
+    len(courses)   #la funcion len() nos devuelve la cantidad de elementos en una lista
+)
+
+
+value = courses[len(courses) - 1]  #len(courses) - 1 nos da el indice del ultimo elemento de la lista
+
+print(
+    value
+)
--- Modulo2_Listas/Listas/main.py
+++ Modulo2_Listas/Listas/main.py
@@ -5,6 +5,10 @@
 #my_list =["String", 10, 3.14, True, ]
 #no usar listos con diferentes tipos de datos
 #usar listas para agrupar datos relacionados
+
+
+#todos los elementos de la listas de ringen por indices y el primer elemento empieza en el indice 0
+#               0        1        2        3        4
 courses = ["Python", "Django", "Flask", "Ruby", "MongoDB"] #strings
 numbers = [10, 20, 30, 40, 50] #enteros
 
